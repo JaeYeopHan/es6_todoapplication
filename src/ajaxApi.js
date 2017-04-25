800f835b1ea08dc6310c83d0747573090e08fffe
Jbee <Jbee@AL01155703.local>
2017-04-25 20:14:37 +0900

    Add ajax feature

--- src/ajaxApi.js
+++ src/ajaxApi.js
@@ -1,14 +1,77 @@
 const get = url => {
     return new Promise((res, rej) => {
         const xhr = new XMLHttpRequest();
-        xhr.open("get", url, true);
+        xhr.open("GET", url, true);
         xhr.responseType = "json";
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     res(xhr.response);
                 } else {
-                    rej(xhr.status);
+                    rej(new Error(xhr.status));
+                }
+            }
+        };
+        xhr.send();
+    });
+};
+
+const post = (url, data) => {
+    return new Promise((res, rej) => {
+        const params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.responseType = "json";
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    res(xhr.response);
+                } else {
+                    rej(new Error(xhr.status));
+                }
+            }
+        };
+        xhr.send(params);
+    });
+};
+
+const put = (url, data) => {
+    return new Promise((res, rej) => {
+        const params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
+        const xhr = new XMLHttpRequest();
+        xhr.open("PUT", url, true);
+        xhr.responseType = "json";
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    res(xhr.response);
+                } else {
+                    rej(new Error(xhr.status));
+                }
+            }
+        };
+        xhr.send(params);
+    });
+};
+
+const del = (url) => {
+    return new Promise((res, rej) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("DELETE", url, true);
+        xhr.responseType = "json";
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    res(xhr.response);
+                } else {
+                    rej(new Error(xhr.status));
                 }
             }
         };
@@ -17,5 +80,5 @@ const get = url => {
 };
 
 export const ajaxApi = {
-    get
+    get, post, put, del
 };
