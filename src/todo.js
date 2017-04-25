e36bc6e64b1c7d8ac9a9c10106278e8af6a4d3f6
Jbee <Jbee@AL01155703.local>
2017-04-25 20:47:36 +0900

    Refactoring

--- src/todo.js
+++ src/todo.js
@@ -1,103 +1,72 @@
-import { ajaxApi as ajax } from './ajaxApi';
+import * as log from "loglevel";
+
+import ajax from './ajaxApi';
+import todoApi from './todoApi';
 
 export default class Todo {
     constructor() {
         this.URL = "http://128.199.76.9:8002/jbee/todo/";
-        this.COMPLETED = 1;
         this.NOT_COMPLETED = 0;
+        this.COMPLETED = 1;
+        this.dom = new todoApi();
     }
 
     init() {
-		this.bindEvent();
-        this.getFetch();
+		this._bindEvent();
+        this._getFetch();
     }
 
-    bindEvent() {
+    _bindEvent() {
 		document.querySelector(".new-todo")
-				.addEventListener("keydown", this.keydownHandler.bind(this));
+				.addEventListener("keydown", this._add.bind(this));
 		document.querySelector(".todo-list")
-				.addEventListener("click", this.destroy.bind(this));
-		document.querySelector(".todo-list")
-			    .addEventListener("click", this.update.bind(this));
+				.addEventListener("click", this._remove.bind(this));
+        document.querySelector(".todo-list")
+                .addEventListener("click", this._update.bind(this));
 	}
 
-	getFetch() {
-        ajax.get(this.URL)
-            .then((data) => this.addTodo(this.merge(document.createDocumentFragment(), data),
-                () => console.log("fetch data success")))
-            .catch((e) =>console.log("error: " + e));
+	_getFetch() {
+        return ajax.get(this.URL)
+                    .then((todos) => this.dom.addTodo(this.dom.merge(document.createDocumentFragment(), todos)))
+                    .catch((e) => log.error("error: " + e));
     }
 
-    merge(fragment, data) {
-        data.forEach(d => fragment.appendChild(this.createTemplate(d.id, d.todo, d.completed)));
-        return fragment;
-    }
-
-    //TODO-DOM
-    createTemplate(id, value, completed) {
-        const template = `
-			<div class="view">
-				<input type="checkbox" class="toggle"/>
-				<label>${value}</label>
-				<button class="destroy"></button>
-				<input class="edit" value="${value}"/>
-			</div>`;
-        let li = document.createElement("li");
-        li.setAttribute("data-id", id);
-        if (completed) {
-            li.classList.add("completed");
-        }
-        li.innerHTML = template;
-        return li;
-    }
-
-    //TODO-DOM
-    addTodo(template, clear) {
-        document.querySelector(".todo-list").appendChild(template);
-        clear();
-    }
-
-	destroy(e) {
+	_remove(e) {
         const id = e.target.parentNode.parentNode.getAttribute("data-id");
-        ajax.del(this.URL + id)
-            .then(() => this.deleteTodo(e.target.classList, e.target.parentNode.parentNode))
-            .catch((e) => console.log("error: ", e));
-
+        if (e.target.classList.contains("destroy")) {
+            return ajax.del(this.URL + id)
+                        .then(() => this.dom.removeTodo(e.target.classList, e.target.parentNode.parentNode))
+                        .catch((e) => log.error("error: ", e));
+        }
 	}
 
-	//TODO-DOM
-	deleteTodo(classList, todo) {
-        if (classList.contains("destroy")) {
-            todo.remove();
-        }
-    }
-
-	update(e) {
-        const todoClassList = e.target.parentNode.parentNode.classList;
-        const id = e.target.parentNode.parentNode.getAttribute("data-id");
+	_update(e) {
+        const todoElm = e.target.parentNode.parentNode;
+        const id = todoElm.getAttribute("data-id");
         let data = {
-            "completed" : !todoClassList.contains("completed") + 0
+            "completed" : todoElm.classList.contains("completed")
+                        ? this.NOT_COMPLETED : this.COMPLETED
         };
 
-        ajax.put(this.URL + id, data)
-            .then(() => this.updateTodoStatus(e.target.classList, todoClassList))
-            .catch((e) => console.log("error: ", e));
+        return ajax.put(this.URL + id, data)
+                    .then(() => this.dom.updateTodoStatus(e.target.classList, todoElm.classList))
+                    .catch((e) => log.error("error: ", e));
 	}
 
-	//TODO-DOM
-	updateTodoStatus(classList, todoClassList) {
-        if (classList.contains("toggle")) {
-            todoClassList.toggle("completed");
+    _add(e) {
+        const value = e.target.value;
+        if (this._isCorrectInput(e)) {
+            return ajax.post(this.URL, { "todo" : value })
+                        .then((res) => this.dom.addTodo(this.dom.createTemplate({
+                                id : res.insertId,
+                                todo : value,
+                                completed : this.NOT_COMPLETED
+                        }), () => e.target.value = ""))
+                        .catch((e) => log.error("error: ", e));
         }
     }
 
-    keydownHandler(e) {
-        const value = e.target.value;
-        if (e.which === 13 && value.trim() !== "") {
-            ajax.post(this.URL, { "todo" : value })
-                .then((res) => this.addTodo(this.createTemplate(res.insertId, value, this.NOT_COMPLETED),
-                    () => e.target.value = ""))
-                .catch((e) => console.log("error: ", e));
-        }
+    _isCorrectInput(e) {
+        return e.which === 13 && e.target.value.trim() !== "";
     }
 }
